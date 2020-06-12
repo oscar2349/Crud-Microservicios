7e276d47cb95d2bea90f0d7a7c341b6e471513bd
OscarRAmirez <oerm16@gmail.com>
2020-06-12 06:54:12 -0500

    inicio

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/service/IProductoService.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/service/IProductoService.java
@@ -7,6 +7,7 @@ import com.formacionbdi.springboot.app.productos.models.entity.Producto;
 public interface IProductoService {
 
 	public List<Producto> findAll();
+	
 	public Producto findById(Long id);
 	
 	public Producto save(Producto producto);
